7ffa5655d2203a09bd51ba26e28ae6f6e4f92731
preich <preich@192.168.1.101>
2020-01-08 23:32:50 +0000

    added

---
+++ src/main/java/com/github/jazzmuesli/jacevo/Calculator.java
@@ -0,0 +1,12 @@
+package com.github.jazzmuesli.jacevo;
+
+public class Calculator {
+
+	public double divide(double a, double b) {
+		return a / b;
+	}
+
+	public double add(double a, double b) {
+		return a + b;
+	}
+}
